7a89519381aaf4cbee63a3443835821c15c10584
Abdullah Nayer <abdullahnayer@gmail.com>
2023-09-09 04:36:26 +0300

    update task status

--- models/task.js
+++ models/task.js
@@ -1,6 +1,6 @@
 import client from "../db-client.js";
 
-const TaskStore = {
+class TaskStore {
   async getAll() {
     try {
       const conn = await client.connect();
@@ -12,19 +12,72 @@ const TaskStore = {
       console.error(err);
       throw err;
     }
-  },
+  }
+
   async add(title, description, created_by) {
     try {
       const conn = await client.connect();
-      const sql = "INSERT INTO tasks (title, description, created_by) VALUES ($1, $2, $3) RETURNING *";
-      const result = await conn.query(sql, [title, description, created_by]);      
+      const sql =
+        "INSERT INTO tasks (title, description, created_by) VALUES ($1, $2, $3) RETURNING *";
+      const result = await conn.query(sql, [title, description, created_by]);
       conn.release();
       return result.rows[0];
     } catch (err) {
       console.error(err);
       throw err;
     }
-  },
+  }
+
+  async updateStatus(taskId, newStatus, userId) {
+    try {
+      const conn = await client.connect();
+      await conn.query("BEGIN");
+      const find = await conn.query("SELECT * FROM tasks WHERE id = $1", [
+        taskId,
+      ]);
+      const task = find.rows[0];
+      if (!task) throw new Error(`Cannot find task with id ${taskId}`);
+
+      // Update task status and updated_by fields
+      const updateTaskSql =
+        "UPDATE tasks SET status = $1, updated_by = $2 WHERE id = $3 RETURNING *";
+      const updateTaskResult = await conn.query(updateTaskSql, [
+        newStatus,
+        userId,
+        taskId,
+      ]);
+      const updatedTask = updateTaskResult.rows[0];
+
+      // Insert record into status_updates table
+      const insertStatusUpdatesSql =
+        "INSERT INTO status_updates (from_status, to_status) VALUES ($1, $2) RETURNING id";
+      const insertStatusUpdate = await conn.query(insertStatusUpdatesSql, [
+        task.status,
+        newStatus,
+      ]);
+      const statusUpdateId = insertStatusUpdate.rows[0].id;
+
+      // Insert record into changes table
+      const insertChangesSql =
+        "INSERT INTO changes (task_id, user_id, status_update_id) VALUES ($1, $2, $3) RETURNING *";
+      const change = await conn.query(insertChangesSql, [
+        taskId,
+        userId,
+        statusUpdateId,
+      ]);
+
+      await conn.query("COMMIT");
+      conn.release();
+
+      return updatedTask;
+    } catch (err) {
+      await client.query("ROLLBACK");
+      throw err;
+    }
+  }
+
+
+
   async delete(id) {
     try {
       const conn = await client.connect();
@@ -36,7 +89,7 @@ const TaskStore = {
       console.error(err);
       throw err;
     }
-  },
-};
+  }
+}
 
 export default TaskStore;
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import TaskStore from "../models/task.js";
 
 const router = Router();
+const store = new TaskStore();
 
 router.get("/", async (req, res) => {
-  const tasks = await TaskStore.getAll();
+  const tasks = await store.getAll();
   res.send(tasks);
 });
 
@@ -13,14 +14,29 @@ router.post("/", async (req, res) => {
   if (!title) return res.status(400).send("title is required");
   if (!description) return res.status(400).send("description is required");
   if (!user) return res.status(400).send("user is required");
-  const newTask = await TaskStore.add(title, description, user);
+  const newTask = await store.add(title, description, user);
   res.send(newTask);
 });
 
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  await TaskStore.delete(id);
+  await store.delete(id);
   res.send("Task deleted");
 });
+
+router.put("/:id/status", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { user, status } = req.body;
+    if (!user) return res.status(400).send("user is required");
+    const updatedTask = await store.updateStatus(id, status, user);
+    res.send(updatedTask);
+  } catch (error) {
+    console.error(error);
+    res.status(422).send(error.message);
+  }
+});
+
+
 
 export default router;
